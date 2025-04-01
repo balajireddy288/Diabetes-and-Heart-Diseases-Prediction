8e9728c23b39351b04c7207db6eec8255a336ba9
balajireddy228 <reddyb288@gmail.com>
2025-04-01 20:35:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,232 @@
+import streamlit as st
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+import pandas as pd
+from streamlit_option_menu import option_menu
+import pickle
+from PIL import Image
+import numpy as np
+import plotly.figure_factory as ff
+import streamlit as st
+# from code.DiseaseModel import DiseaseModel
+# from code.helper import prepare_symptoms_array
+import seaborn as sns
+import matplotlib.pyplot as plt
+import joblib
+
+# loading the models
+diabetes_model = joblib.load("models/diabetes_model.sav")
+heart_model = joblib.load("models/heart_disease_model.sav")
+
+
+# sidebar
+with st.sidebar:
+    selected = option_menu('Multiple Disease Prediction', [
+        'Diabetes Prediction',
+        'Heart disease Prediction',
+    ],
+        icons=['','activity', 'heart', 'person','person','person','person','bar-chart-fill'],
+        default_index=0)
+
+
+
+
+
+
+# Diabetes prediction page
+if selected == 'Diabetes Prediction':  # pagetitle
+    st.title("Diabetes disease prediction")
+    image = Image.open('d3.jpg')
+    st.image(image, caption='diabetes disease prediction')
+    # columns
+    # no inputs from the user
+    name = st.text_input("Name:")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        Pregnancies = st.number_input("Number of Pregnencies")
+    with col2:
+        Glucose = st.number_input("Glucose level")
+    with col3:
+        BloodPressure = st.number_input("Blood pressure  value")
+    with col1:
+
+        SkinThickness = st.number_input("Sckinthickness value")
+
+    with col2:
+
+        Insulin = st.number_input("Insulin value ")
+    with col3:
+        BMI = st.number_input("BMI value")
+    with col1:
+        DiabetesPedigreefunction = st.number_input(
+            "Diabetespedigreefunction value")
+    with col2:
+
+        Age = st.number_input("AGE")
+
+    # code for prediction
+    diabetes_dig = ''
+
+    # button
+    if st.button("Diabetes test result"):
+        diabetes_prediction=[[]]
+        diabetes_prediction = diabetes_model.predict(
+            [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreefunction, Age]])
+
+        # after the prediction is done if the value in the list at index is 0 is 1 then the person is diabetic
+        if diabetes_prediction[0] == 1:
+            diabetes_dig = "we are really sorry to say but it seems like you are Diabetic."
+            image = Image.open('positive.jpg')
+            st.image(image, caption='')
+        else:
+            diabetes_dig = 'Congratulation,You are not diabetic'
+            image = Image.open('negative.jpg')
+            st.image(image, caption='')
+        st.success(name+' , ' + diabetes_dig)
+        
+        
+
+
+
+# Heart prediction page
+if selected == 'Heart disease Prediction':
+    st.title("Heart disease prediction")
+    image = Image.open('heart2.jpg')
+    st.image(image, caption='heart failuire')
+    # age	sex	cp	trestbps	chol	fbs	restecg	thalach	exang	oldpeak	slope	ca	thal	target
+    # columns
+    # no inputs from the user
+    name = st.text_input("Name:")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        age = st.number_input("Age")
+    with col2:
+        sex=0
+        display = ("male", "female")
+        options = list(range(len(display)))
+        value = st.selectbox("Gender", options, format_func=lambda x: display[x])
+        if value == "male":
+            sex = 1
+        elif value == "female":
+            sex = 0
+    with col3:
+        cp=0
+        display = ("typical angina","atypical angina","non — anginal pain","asymptotic")
+        options = list(range(len(display)))
+        value = st.selectbox("Chest_Pain Type", options, format_func=lambda x: display[x])
+        if value == "typical angina":
+            cp = 0
+        elif value == "atypical angina":
+            cp = 1
+        elif value == "non — anginal pain":
+            cp = 2
+        elif value == "asymptotic":
+            cp = 3
+    with col1:
+        trestbps = st.number_input("Resting Blood Pressure")
+
+    with col2:
+
+        chol = st.number_input("Serum Cholestrol")
+    
+    with col3:
+        restecg=0
+        display = ("normal","having ST-T wave abnormality","left ventricular hyperthrophy")
+        options = list(range(len(display)))
+        value = st.selectbox("Resting ECG", options, format_func=lambda x: display[x])
+        if value == "normal":
+            restecg = 0
+        elif value == "having ST-T wave abnormality":
+            restecg = 1
+        elif value == "left ventricular hyperthrophy":
+            restecg = 2
+
+    with col1:
+        exang=0
+        thalach = st.number_input("Max Heart Rate Achieved")
+   
+    with col2:
+        oldpeak = st.number_input("ST depression induced by exercise relative to rest")
+    with col3:
+        slope=0
+        display = ("upsloping","flat","downsloping")
+        options = list(range(len(display)))
+        value = st.selectbox("Peak exercise ST segment", options, format_func=lambda x: display[x])
+        if value == "upsloping":
+            slope = 0
+        elif value == "flat":
+            slope = 1
+        elif value == "downsloping":
+            slope = 2
+    with col1:
+        ca = st.number_input("Number of major vessels (0–3) colored by flourosopy")
+    with col2:
+        thal=0
+        display = ("normal","fixed defect","reversible defect")
+        options = list(range(len(display)))
+        value = st.selectbox("thalassemia", options, format_func=lambda x: display[x])
+        if value == "normal":
+            thal = 0
+        elif value == "fixed defect":
+            thal = 1
+        elif value == "reversible defect":
+            thal = 2
+    with col3:
+        agree = st.checkbox('Exercise induced angina')
+        if agree:
+            exang = 1
+        else:
+            exang=0
+    with col1:
+        agree1 = st.checkbox('fasting blood sugar > 120mg/dl')
+        if agree1:
+            fbs = 1
+        else:
+            fbs=0
+    # code for prediction
+    heart_dig = ''
+    
+
+    # button
+    if st.button("Heart test result"):
+        heart_prediction=[[]]
+        # change the parameters according to the model
+        
+        # b=np.array(a, dtype=float)
+        heart_prediction = heart_model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+
+        if heart_prediction[0] == 1:
+            heart_dig = 'we are really sorry to say but it seems like you have Heart Disease.'
+            image = Image.open('positive.jpg')
+            st.image(image, caption='')
+            
+        else:
+            heart_dig = "Congratulation , You don't have Heart Disease."
+            image = Image.open('negative.jpg')
+            st.image(image, caption='')
+        st.success(name +' , ' + heart_dig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
